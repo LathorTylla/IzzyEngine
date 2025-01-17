0fd676fb543ee04af23adaa74d49f5906fb3039e
LathorTylla <fabianmurillo1802@gmail.com>
2025-01-17 11:20:22 -0600

    Window Class Implementation

--- IzzyEngine/Include/Prerequisites.h
+++ IzzyEngine/Include/Prerequisites.h
@@ -15,30 +15,47 @@
 #include <D3DX11.h>
 #include <d3dcompiler.h>
 #include "resource.h"
+#include "Resource.h"
 
+//Macros
+#define SAFE_RELEASE(x) if(x != nullptr) x->Release(); x = nullptr;
 
+#define MESSAGE( classObj, method, state )   \
+{                                            \
+   std::wostringstream os_;                  \
+   os_ << classObj << "::" << method << " : " << "[CREATION OF RESOURCE " << ": " << state << "] \n"; \
+   OutputDebugStringW( os_.str().c_str() );  \
+}
+
+#define ERROR( classObj, method, errorMSG )  \
+{                                            \
+   std::wostringstream os_;                  \
+   os_ << "ERROR : " << classObj << "::" << method << " : " << "  Error in data from params [" << errorMSG << "] \n"; \
+   OutputDebugStringW( os_.str().c_str() );  \
+   exit(1);                                  \
+}
 
 //--------------------------------------------------------------------------------------
 // Structures
 //--------------------------------------------------------------------------------------
-struct SimpleVertex
-{
+struct 
+SimpleVertex{
   XMFLOAT3 Pos;
   XMFLOAT2 Tex;
 };
 
-struct CBNeverChanges
-{
+struct 
+CBNeverChanges{
   XMMATRIX mView;
 };
 
-struct CBChangeOnResize
-{
+struct 
+CBChangeOnResize{
   XMMATRIX mProjection;
 };
 
-struct CBChangesEveryFrame
-{
+struct 
+CBChangesEveryFrame{
   XMMATRIX mWorld;
   XMFLOAT4 vMeshColor;
 };

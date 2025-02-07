827a11946a3afff0d85832e22bff6c5734879c52
LathorTylla <fabianmurillo1802@gmail.com>
2025-02-07 16:02:15 -0600

    Texture Class
    Implementacion de la clase texture, se añadio un archivo .h stb_image

--- IzzyEngine/Include/Prerequisites.h
+++ IzzyEngine/Include/Prerequisites.h
@@ -59,3 +59,10 @@ CBChangesEveryFrame{
   XMMATRIX mWorld;
   XMFLOAT4 vMeshColor;
 };
+
+enum 
+ExtensionType {
+  DDS = 0,
+  PNG = 1,
+  JPG = 2
+};
--- IzzyEngine/Include/SwapChain.h
+++ IzzyEngine/Include/SwapChain.h
@@ -9,12 +9,12 @@ class Window;
 class Texture;
 
 class 
-swapChain{
+Swapchain{
 public:
-	swapChain() = default;
-	~swapChain() = default;
+	Swapchain() = default;
+	~Swapchain() = default;
 
-	void
+	HRESULT
 		init(Device& device,
 				 DeviceContext& deviceContext,
 				 Texture& backBuffer,
@@ -33,7 +33,7 @@ public:
 	present();
 
 public:
-	IDXGISwapChain* g_pSwapChain = nullptr;
+	IDXGISwapChain* m_swapChain = nullptr;
 	D3D_DRIVER_TYPE m_driverType = D3D_DRIVER_TYPE_NULL;
 private:
 	D3D_FEATURE_LEVEL m_featureLevel = D3D_FEATURE_LEVEL_11_0;

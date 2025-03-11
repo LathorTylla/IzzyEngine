0128bc2329ed77374188667604c0184f076d86b9
LathorTylla <fabianmurillo1802@gmail.com>
2025-03-11 01:02:10 -0600

    SamplerState y Main a BaseApp

--- IzzyEngine/Include/BaseApp.h
+++ IzzyEngine/Include/BaseApp.h
@@ -1,5 +1,17 @@
 #pragma once
 #include "Prerequisites.h"
+#include "Window.h"
+#include "Device.h"
+#include "DeviceContext.h"
+#include "Swapchain.h"
+#include "Texture.h"
+#include "RenderTargetView.h"
+#include "DepthStencilView.h"
+#include "Viewport.h"
+#include "ShaderProgram.h"
+#include "Buffer.h"
+#include "MeshComponent.h"
+#include "SamplerState.h"
 
 /*
  * @brief BaseApp.
@@ -59,5 +71,36 @@ public:
 			int nCmdShow,
 			WNDPROC wndproc);
 
-private:
+public:
+  // Global Variables
+  Window															g_window;
+  Device															g_device;
+  DeviceContext												g_deviceContext;
+  SwapChain														g_swapchain;
+  Texture															g_backBuffer;
+  Texture															g_depthStencil;
+  RenderTargetView										g_renderTargetView;
+  DepthStencilView										g_depthStencilView;
+  Viewport														g_viewport;
+  ShaderProgram												g_shaderProgram;
+
+  Buffer															g_vertexBuffer;
+  Buffer															g_indexBuffer;
+  Buffer 															g_neverChanges;
+  Buffer 															g_changeOnResize;
+  Buffer 															g_changeEveryFrame;
+  Texture 														g_textureCubeImg;
+  SamplerState 												g_samplerState;
+  //ID3D11ShaderResourceView*           g_pTextureRV = nullptr;
+  //ID3D11SamplerState*                 g_pSamplerLinear = nullptr;
+  XMMATRIX                            g_World;
+  XMMATRIX                            g_View;
+  XMMATRIX                            g_Projection;
+  XMFLOAT4                            g_vMeshColor;
+
+  CBChangesEveryFrame cb;
+  CBNeverChanges cbNeverChanges;
+  CBChangeOnResize cbChangesOnResize;
+
+  MeshComponent m_meshComponent;
 };
